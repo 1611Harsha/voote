0991aa5c5da5c2cc8d27dc40bb2070b84fa8e00a
HARSHA A V <harshaacv@gmail.com>
2025-06-05 13:50:27 +0530

    new updaee

--- myscripts.js
+++ myscripts.js
@@ -1,37 +1,67 @@
-function disableButton(Buttonclicked){
-    console.log(Buttonclicked.id)
+
+let count2 = 0;
+  function increaseCount1(buttonNumber1) {
+    if ((buttonNumber1 === 1)){
+      count2++;
+      document.getElementById('counter_1').innerText = count2;
+    }
+      if ((buttonNumber1 === 2)){
+      count2++;
+      document.getElementById('counter_2').innerText = count2;
+      
+}
+      if ((buttonNumber1 === 3)){
+      count2++;
+      document.getElementById('counter_3').innerText = count2;
+      
+}
+
+      if ((buttonNumber1 === 4)){
+      count2++;
+      document.getElementById('counter_4').innerText = count2;
+      
+}
+console.log(buttonNumber1.id)
     const allbtn = document.querySelectorAll('.card__btn');
     console.log(allbtn)
     allbtn.forEach((buttonInfo,index)=>{
         console.log(buttonInfo .id)
-        if(Buttonclicked.id !== buttonInfo.id){ 
+        if(buttonNumber1.id !== buttonInfo.id){ 
             buttonInfo.disabled =true;
         }
-    });
-    for (let i = 1; i <= 4; i++) {
-    let count = 0;
-    document.getElementById(`b${i}`).addEventListener("click", function () {
-        count++;
-        document.getElementById(`counter_${i}`).innerText = count;
-    });
+         });
+  }     
+
+
+ let count1 = 0;
+  function increaseCount(buttonNumber) {
+    if ((buttonNumber === 1)){
+      count1++;
+      document.getElementById('counter1_1').innerText = count1;
+    }
+      if ((buttonNumber === 2)){
+      count1++;
+      document.getElementById('counter1_2').innerText = count1;
+      
 }
+      if ((buttonNumber === 3)){
+      count1++;
+      document.getElementById('counter1_3').innerText = count1;
+      
 }
-function disableButton2(Buttonclicked1){
-    console.log(Buttonclicked1.id)
+
+      if ((buttonNumber === 4)){
+      count1++;
+      document.getElementById('counter1_4').innerText = count1;
+      
+}
+console.log(buttonNumber.id)
     const allbtn = document.querySelectorAll('.card__btn1');
     console.log(allbtn)
     allbtn.forEach((buttonInfo,index)=>{
         console.log(buttonInfo .id)
-        if(Buttonclicked1.id !== buttonInfo.id){ 
+        if(buttonNumber.id !== buttonInfo.id){ 
             buttonInfo.disabled =true;
         }
-    });
- for (let i = 1; i <= 4; i++) {
-    let count = 0;
-    document.getElementById(`v${i}`).addEventListener("click", function () {
-        count++;
-        document.getElementById(`counter1_${i}`).innerText = count;
-    });
-}
-}
- 
+         });
+  }     
